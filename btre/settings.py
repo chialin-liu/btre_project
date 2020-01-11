3537916290e825bd9115e46f8ccdbe11e0f22bb5
liuchialin <charles.ee96@g2.nctu.edu.tw>
2020-01-11 17:11:56 +0800

    add local setting

--- btre/settings.py
+++ btre/settings.py
@@ -139,5 +139,8 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
